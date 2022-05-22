320b224240775a936f8f7db898c0ac959883402b
Aakash <aakashdhanyamraju425@gmail.com>
2022-05-22 18:42:14 +0530

    created public and private routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import 'rsuite/dist/rsuite.min.css';
 
-import './App.css';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute'
 import './styles/main.scss'
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
   return (
-    <div >
-      Hello
-    </div>
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+
+      <PrivateRoute path='/' >
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
