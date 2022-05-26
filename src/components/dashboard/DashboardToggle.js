b067d6db080427cd9ac6bd71ce3de8c770980e27
Aakash Dhanyamraju <aakashdhanyamraju425@gmail.com>
2022-05-26 09:56:53 +0530

    signout and in coded

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Button,Drawer,Icon } from 'rsuite'
+import React, { useCallback } from 'react'
+import { Alert, Button,Drawer,Icon } from 'rsuite'
 import Dashboard from '.'
 import { useMediaQuery, useModelState } from '../../misc/custom-hooks'
 
@@ -7,7 +7,16 @@ const DashboardToggle = () => {
 
     const {open,isOpen,close} = useModelState()
     const isMobile=useMediaQuery('(max-width:992px)')
+    const onSignOut = useCallback(
+      () => {
+        auth.signOut()
 
+        Alert.info("Sign out",4000)
+        close()
+      },
+      [close],
+    )
+    
 
 
     
@@ -17,7 +26,7 @@ const DashboardToggle = () => {
             <Icon icon="dashboard" />Dashboard
         </Button>
         <Drawer full={isMobile} show={isOpen} onHide={close} placement='left' >
-            <Dashboard />
+            <Dashboard onSignOut={onSignOut}/>
         </Drawer>
     </>
   )
