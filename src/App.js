960ea912c116b4ca337660976283f6c5abec611f
Aakash Dhanyamraju <aakashdhanyamraju425@gmail.com>
2022-05-30 11:53:26 +0530

    Added cropable avatar and displayed it in dashboard

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import SignIn from './pages/SignIn';
 import PrivateRoute from './components/PrivateRoute'
-import './styles/main.scss'
+import './styles/main.scss';
+import './styles/utility.scss'
 import Home from './pages/Home';
 import PublicRoute from './components/PublicRoute';
 import 'rsuite/dist/styles/rsuite-default.css';
 import { ProfileProvider } from './context/profile.context';
+
 
 
 
--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -1,18 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState,useRef } from 'react'
 import { Alert, Button, Modal } from 'rsuite'
 import AvatarEditor from 'react-avatar-editor'
 import { useModelState } from '../../misc/custom-hooks'
+import { database, storage } from '../../misc/firebase'
+import ProfileAvatar from './ProfileAvatar'
+import { useProfile } from '../../context/profile.context'
+import '../../styles/utility.scss'
 
 
 const fileInputTypes=".png,.jpeg,.jpg"
-
 const acceptedFileTypes=['image/png','image/jpeg','image/pjpeg']
 const isValidFile=(file)=> acceptedFileTypes.includes(file.type)
 
 
+const getBlob =(canvas)=>{
+  return new Promise((resolve, reject)=>{
+    canvas.toBlob((blob)=>{
+      if(blob){
+        resolve(blob)
+
+      }else{
+        reject(new Error('file process error'))
+      }
+    })
+  })
+}
+
+
 const AvatarUploadBtn = () => {
+
   const {isOpen,open,close} =useModelState()
+  const {profile}=useProfile()
   const [img,setImg] = useState(null)
+  const avatarEditorRef=useRef()
+  const [isLoading,setIsLoading]= useState(false)
 
   const onFileinputChange=(ev)=>{
 
@@ -29,10 +50,34 @@ const AvatarUploadBtn = () => {
     }
   }
 
+  const onUploadClick=async()=>{
+      const canvas =avatarEditorRef.current.getImageScaledToCanvas()
 
+      setIsLoading(true)
+      try {
+
+          const blob = await getBlob(canvas)
+          const avatarFileRef = storage.ref(`/profile/${profile.uid}`).child('avatar')
+
+          const uploadAvatarResult=await avatarFileRef.put(blob,{
+            cacheControl:`public,max-age=${3600*24*3}`
+          })
+          const downloadUrl = await uploadAvatarResult.ref.getDownloadURL()
+
+          const userAvatarRef = database.ref(`/profiles/${profile.uid}`).child('avatar')
+          userAvatarRef.set(downloadUrl)
+          setIsLoading(false)
+          Alert.info('Avatar has been uploaded',4000)
+
+      } catch (err) {
+        setIsLoading(false)
+        Alert.error(err.message,4000)
+      }
+  }
 
   return (
     <div className='mt-3 text-center' >
+      <ProfileAvatar src={profile.avatar} name={profile.name} className="width-200 height-200 font-huge img-fullsize" />
       <div>
         <label htmlFor='avatar-upload' className='d-block cursor-pointer padded'  >
           select new Avatar
@@ -40,11 +85,12 @@ const AvatarUploadBtn = () => {
         </label>
 
         <Modal show={isOpen} onHide={close} >
-          <Modal.Header>head</Modal.Header>
+            <Modal.Header><Modal.Title>Uplaod avatar</Modal.Title></Modal.Header>
           <Modal.Body>
             <div className='d-flex justify-content-center align-items-center h-100' >
             {img &&
             <AvatarEditor
+              ref={avatarEditorRef}
               image={img}
               width={200}
               height={200}
@@ -56,7 +102,7 @@ const AvatarUploadBtn = () => {
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <Button block appearance='ghost' >
+            <Button block appearance='ghost' onClick={onUploadClick} disabled={isLoading} >
               Upload new Avatar
             </Button>
           </Modal.Footer>
