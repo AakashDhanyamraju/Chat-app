515f5efa5103f6f5b2d15aa65bc74fcd079bc9d6
Aakash Dhanyamraju <aakashdhanyamraju425@gmail.com>
2022-05-31 12:19:36 +0530

    created room context

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useRef, useState,useEffect } from 'react'
+import { Divider } from 'rsuite'
 import CreateRoomBtnModel from './CreateRoomBtnModel'
 import DashboardToggle from './dashboard/DashboardToggle'
+import ChatRoomList from './rooms/ChatRoomList'
+
 
 export const Sidebar = () => {
+  const topSidebarRef=useRef()
+  const [height,seHeight]=useState(0)
+
+  useEffect(() => {
+    if(topSidebarRef.current){
+      seHeight(topSidebarRef.current.scrollHeight)
+    }
+  }, [topSidebarRef])
+
   return (
     <div className='h-100 pt-2'>
         <div>
             <DashboardToggle />
             <CreateRoomBtnModel />
+            <Divider>Join Conversation</Divider>
         </div>
 
-        bottom
+        <ChatRoomList aboveElHeight={height} />
     </div>
   )
 }
